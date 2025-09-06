4bd3fc01db101c8e95009f0847702287a92a0fd6
mscm10 <mscorderom@ufpso.edu.co>
2025-09-06 07:20:19 -0500

    ejercicio basura

---
+++ src/operadoresPreguntas/forclass.java
@@ -0,0 +1,18 @@
+package operadoresPreguntas;
+
+public class forclass {
+   int num1 = 3;
+   int num2 = 5;
+   int num3 = 1;
+   int numMax
+   for(int i = 0; i < 5; i ++){
+    if (num1>num2 && num1>num3){
+        numMax=num1;
+    }else  if (num2>num1 && num2>num3){
+        numMax=num2
+    }else {
+        numMax=num3;
+    }
+    
+   }
+}
